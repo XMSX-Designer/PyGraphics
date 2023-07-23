85fb6c9016bfd3a8161bcd5ef80d71dbf1f3c109
XMSX-Designer <110811743+XMSX-Designer@users.noreply.github.com>
2023-07-23 15:29:07 -0500

    Update osutil_windows.py

--- graphics/osutil_windows.py
+++ graphics/osutil_windows.py
@@ -1,8 +1,19 @@
 from win32api import GetSystemMetrics
 
-class display:
-    
+class Display:
+    """
+    A class representing display information and providing a static method to get the screen size.
+
+    Methods:
+        get_size(): Get the screen size as a tuple (width, height).
+    """
+
     @staticmethod
-    def getsize():
-        return GetSystemMetrics(0),GetSystemMetrics(1)
-        
+    def get_size():
+        """
+        Get the screen size using the GetSystemMetrics function from the win32api library.
+
+        Returns:
+            tuple: A tuple containing the screen width and height in pixels.
+        """
+        return GetSystemMetrics(0), GetSystemMetrics(1)
