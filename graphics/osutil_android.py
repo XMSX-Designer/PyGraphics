50d02cf500120ce090b39d0f26bd547170080d43
XMSX-Designer <110811743+XMSX-Designer@users.noreply.github.com>
2023-07-23 15:29:50 -0500

    Update osutil_android.py

--- graphics/osutil_android.py
+++ graphics/osutil_android.py
@@ -1,17 +1,27 @@
-
 from jnius import autoclass
 
-    
-class display:
+class Display:
+    """
+    A class representing display information for Android devices and providing a static method to get the screen size.
+
+    Methods:
+        get_size(): Get the screen size as a tuple (width, height) in pixels.
+    """
+
     InputMethodManager = autoclass("android.view.inputmethod.InputMethodManager")
     PythonActivity = autoclass("org.kivy.android.PythonActivity")
     Context = autoclass("android.content.Context")
     activity = PythonActivity.mActivity
-    
+
     @staticmethod
-    def getsize():
-        rootView = display.activity.getWindow().getDecorView()
-        rect=autoclass('android.graphics.Rect')()
+    def get_size():
+        """
+        Get the screen size using JNI (Java Native Interface) to interact with Android methods.
+
+        Returns:
+            tuple: A tuple containing the screen width and height in pixels.
+        """
+        rootView = Display.activity.getWindow().getDecorView()
+        rect = autoclass('android.graphics.Rect')()
         rootView.getWindowVisibleDisplayFrame(rect)
-        return rect.right,rect.bottom-rect.top
-        
+        return rect.right, rect.bottom - rect.top
